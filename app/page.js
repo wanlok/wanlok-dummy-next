7ba9f9ad9a614591436dd6c55fafa0bda7c15e17
Robert Wan <wantunglok@gmail.com>
2024-05-07 18:25:56 +1000

    add image slideshow

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
-import classes from './page.module.css';
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
         <div className={classes.slideshow}>
-          
+          <ImageSlideshow />
         </div>
         <div>
           <div className={classes.hero}>
@@ -25,8 +26,8 @@ export default function Home() {
           <h2>How it works</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
@@ -38,14 +39,14 @@ export default function Home() {
           <h2>Why NextLevel Food?</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
-        </section>        
+        </section>
       </main>
     </>
   );
