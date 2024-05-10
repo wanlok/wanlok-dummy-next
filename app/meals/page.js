02e9ab788fc2f81f2a977637cd3b3c6b2cf48050
Robert Wan <wantunglok@gmail.com>
2024-05-10 23:03:55 +1000

    fetch data from local database

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,10 @@ import MealsGrid from "@/components/meals/meals-grid";
 import Link from "next/link";
 
 import classes from "./page.module.css";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +21,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
